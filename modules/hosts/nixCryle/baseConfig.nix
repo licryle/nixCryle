da9456843dc48db2ee35f34802d8488fde9417fa
Cyrille Berliat <cyrille@berliat.fr>
2026-04-05 03:15:51 +0800

    Use american language in UI.

--- modules/hosts/nixCryle/baseConfig.nix
+++ modules/hosts/nixCryle/baseConfig.nix
@@ -14,7 +14,7 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
-    i18n.defaultLocale = "fr_FR.UTF-8";
+    i18n.defaultLocale = "en_US.UTF-8";
     i18n.extraLocaleSettings = {
       LC_ADDRESS = "fr_FR.UTF-8";
       LC_IDENTIFICATION = "fr_FR.UTF-8";
