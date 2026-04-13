89c209889795c4db1b1327881225aaa8a142bc84
Cyrille Berliat <cyrille@berliat.fr>
2026-04-13 13:12:33 +0800

    Add Apple MacBookPro 2012 target.

---
+++ modules/hosts/proCryle.nix
@@ -0,0 +1,32 @@
+{ inputs, self, user, nixOsVersion, keyboardLayout, ... }: {
+  flake.nixosConfigurations.proCryle = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.baseSystem
+      {
+        networking.hostName = "proCryle";
+      }
+
+      ({ lib, pkgs, ... }: {
+        nixpkgs.config = {
+          allowUnfree = true;
+          allowInsecurePredicate = pkg: lib.elem (lib.getName pkg) [
+            "broadcom-sta"
+          ];
+        };
+
+        boot = {
+          kernelModules = [ "wl" ];
+          extraModulePackages = [ pkgs.linuxPackages.broadcom_sta ];
+        };
+
+        services.xserver.videoDrivers = [ "intel" ];
+        hardware.graphics.enable = true;
+        hardware.facetimehd.enable = true;
+      })
+    ];
+
+    specialArgs = {
+      inherit inputs self user nixOsVersion keyboardLayout;
+    };
+  };
+}
