990c6ea473597f17ba6d8a5d94c31fe359fc4bf0
Cyrille Berliat <cyrille@berliat.fr>
2026-04-21 22:25:14 +0800

    Add airCryle config for mba2012.

---
+++ modules/hosts/airCryle.nix
@@ -0,0 +1,32 @@
+{ inputs, self, user, nixOsVersion, keyboardLayout, ... }: {
+  flake.nixosConfigurations.proCryle = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.baseSystemImpureHardware
+      {
+        networking.hostName = "proCryle";
+      }
+
+      ({ lib, pkgs, ... }: {
+        nixpkgs.config = {
+          allowUnfree = true;
+          allowInsecurePredicate = pkg: lib.elem (lib.getName pkg) [
+            "broadcom-sta"
+          ];
+        };
+
+        boot = {
+          kernelModules = [ "wl" ];
+          extraModulePackages = [ pkgs.linuxPackages.broadcom_sta ];
+        };
+        boot.blacklistedKernelModules = [ "b43" "bcma" ];
+
+        services.xserver.videoDrivers = [ "intel" ];
+        hardware.graphics.enable = true;
+      })
+    ];
+
+    specialArgs = {
+      inherit inputs self user nixOsVersion keyboardLayout;
+    };
+  };
+}
