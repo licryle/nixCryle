e72752b6132ddeaa9ecf8e5fb1f23a25ad9633dc
Cyrille Berliat <cyrille@berliat.fr>
2026-05-11 16:40:35 +0800

    Add SD target for proCryle.

--- modules/hosts/proCryle.nix
+++ modules/hosts/proCryle.nix
@@ -1,24 +1,51 @@
-{ inputs, self, user, nixOsVersion, keyboardLayout, ... }: {
-  # sudo nixos-rebuild switch --flake .#proCryle --impure
-  flake.nixosConfigurations.proCryle = inputs.nixpkgs.lib.nixosSystem {
-    modules = [
-      self.nixosModules.impureHardware
-      self.nixosModules.metalHardware
-      self.nixosModules.appleIntelHardware
-      {
-        hardware.graphics.enable = true;
-      }
+{ inputs, self, user, nixOsVersion, keyboardLayout, ... }:
 
-      self.nixosModules.tuiSystem
+let
+  proCryleModules = [
+    self.nixosModules.appleIntelHardware
+    {
+      hardware.graphics.enable = true;
+    }
 
-      self.nixosModules.niriDesktop
-      {
-        networking.hostName = "proCryle";
-      }
-    ];
+    self.nixosModules.tuiSystem
 
-    specialArgs = {
-      inherit inputs self user nixOsVersion keyboardLayout;
+    self.nixosModules.niriDesktop
+    {
+      networking.hostName = "proCryle";
+    }
+  ];
+
+  specialArgs = {
+    inherit inputs self user nixOsVersion keyboardLayout;
+  };
+
+in {
+# sudo nixos-rebuild switch --flake .#proCryle --impure
+  flake.nixosConfigurations = {
+    proCryle = inputs.nixpkgs.lib.nixosSystem {
+      modules =
+        proCryleModules
+        ++ [ 
+          self.nixosModules.impureHardware
+          self.nixosModules.metalHardware
+        ];
+
+      inherit specialArgs;
+    };
+  
+    proCryleSD = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        proCryleModules
+        ++ [
+          "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-x86_64.nix"
+
+          {
+            image.fileName = "proCryle.img";
+          }
+        ];
+
+      inherit specialArgs;
     };
   };
 }
